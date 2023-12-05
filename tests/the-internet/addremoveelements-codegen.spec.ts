7a6f59aa61350c70069d2a5dc613ede878162840
KungRaseri <countjoshington@gmail.com>
2023-12-05 11:43:19 -0800

    added add/remove elements test, flaky in chromium and edge, works in firefox.

--- tests/the-internet/addremoveelements-codegen.spec.ts
+++ tests/the-internet/addremoveelements-codegen.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Add/Remove Elements', async ({ page }) => {
   await page.goto('http://the-internet.herokuapp.com/');
   await page.getByRole('link', { name: 'Add/Remove Elements' }).click();
   await page.getByRole('button', { name: 'Add Element' }).click();
@@ -8,5 +8,5 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Add Element' }).click();
   await page.getByRole('button', { name: 'Delete' }).nth(1).click();
 
-  expect(page.locator('#elements > button')).toHaveCount(2)
+  expect(page.getByRole('button', {name: 'Delete'})).toHaveCount(2);
 });
