58ae7b01bfad870460784a53125f99623132d358
KungRaseri <countjoshington@gmail.com>
2023-12-06 12:48:21 -0800

    added playwright C# implementation, added github action for dotnet build/test

---
+++ dotnet/Playwright.Tests/automation-exercise/AutomationExerciseTest/AutomationExerciseTests.cs
@@ -0,0 +1,15 @@
+namespace Playwright.Tests;
+
+public class AutomationExercise
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ dotnet/Playwright.Tests/the-internet/TheInternetTests.cs
@@ -0,0 +1,15 @@
+namespace Playwright.Tests;
+
+public class TheInternetTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
